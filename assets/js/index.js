bc628526b2b8144abe7b3541cab48bf1000e1d59
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2018-01-22 23:40:45 -0700

    initial site files

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+
+
+$("#linkToAbout").click(function() { 
+	$('html,body').animate({
+        scrollTop: $("#about").offset().top},800);           
+});
+
+$("#linkToRates").click(function() { 
+	$('html,body').animate({
+        scrollTop: $("#rates").offset().top},800);           
+});
+
+$("#linkToEvents").click(function() { 
+	$('html,body').animate({
+        scrollTop: $("#eventsCalendar").offset().top},800);           
+});
+
+$("#linkToPartners").click(function() { 
+	$('html,body').animate({
+        scrollTop: $("#partners").offset().top},800);           
+});
+
+$("#linkToContact").click(function() { 
+	$('html,body').animate({
+        scrollTop: $("#contact").offset().top},800);           
+});
